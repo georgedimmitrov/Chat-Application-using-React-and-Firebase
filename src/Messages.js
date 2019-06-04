6a6fa53a6a5029ed25f6570a64453aa795874f5e
Georgi Dimitrov <dgeorgi@vmware.com>
2019-06-04 23:14:16 +0300

    React chat app - added scroll to bottom when screen is full of messages, dynamic topic, channel and dates.

--- src/Messages.js
+++ src/Messages.js
@@ -1,19 +1,30 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
+import formatDate from 'date-fns/format';
 import useCollection from './utils/useCollection';
-import { db } from './firebase';
+import useDocWithCache from './useDocWithCache';
+import isSameDay from 'date-fns/is_same_day';
+
+function useChatScroll(ref) {
+  useEffect(() => {
+    const node = ref.current;
+    node.scrollTop = node.scrollHeight;
+  });
+}
 
 function Messages({ channelId }) {
   const messages = useCollection(`channels/${channelId}/messages`, 'createdAt');
 
+  const scrollerRef = useRef();
+  useChatScroll(scrollerRef);
+
   return (
-    <div className="Messages">
+    <div ref={scrollerRef} className="Messages">
       <div className="EndOfMessages">That's every message!</div>
 
       {messages.map((message, index) => {
         const previousMessage = messages[index - 1];
-        const showDay = false;
-        const showAvatar =
-          !previousMessage || message.user.id !== previousMessage.user.id;
+        const showDay = shouldShowDay(previousMessage, message);
+        const showAvatar = shouldShowAvatar(previousMessage, message);
 
         return showAvatar ? (
           <FirstMessageFromUser
@@ -33,29 +44,17 @@ function Messages({ channelId }) {
   );
 }
 
-function useDoc(path) {
-  const [doc, setDoc] = useState();
-  useEffect(() => {
-    db.doc(path).onSnapshot(doc => {
-      setDoc({
-        ...doc.data(),
-        id: doc.id
-      });
-    });
-  });
-
-  return doc;
-}
-
 function FirstMessageFromUser({ message, showDay }) {
-  const author = useDoc(message.user.path);
+  const author = useDocWithCache(message.user.path);
 
   return (
     <div key={message.id}>
       {showDay && (
         <div className="Day">
           <div className="DayLine" />
-          <div className="DayText">12/6/2018</div>
+          <div className="DayText">
+            {new Date(message.createdAt.seconds * 1000).toLocaleDateString()}
+          </div>
           <div className="DayLine" />
         </div>
       )}
@@ -69,13 +68,44 @@ function FirstMessageFromUser({ message, showDay }) {
         <div className="Author">
           <div>
             <span className="UserName">{author && author.displayName}</span>{' '}
-            <span className="TimeStamp">3:37 PM</span>
+            <span className="TimeStamp">
+              {formatDate(message.createdAt.seconds * 1000, 'h:mm A')}
+            </span>
           </div>
           <div className="MessageContent">{message.text}</div>
         </div>
       </div>
     </div>
   );
+}
+
+function shouldShowDay(previousMessage, message) {
+  const isFirst = !previousMessage;
+  if (isFirst) {
+    return true;
+  }
+
+  const isNewDay = !isSameDay(
+    previousMessage.createdAt.seconds * 1000,
+    message.createdAt.seconds * 1000
+  );
+  return isNewDay;
+}
+
+function shouldShowAvatar(previousMessage, message) {
+  const isFirst = !previousMessage;
+  if (isFirst) {
+    return true;
+  }
+
+  const differentUser = message.user.id !== previousMessage.user.id;
+  if (differentUser) {
+    return true;
+  }
+
+  const hasBeenAWhile =
+    message.createdAt.seconds - previousMessage.createdAt.seconds > 60 * 3;
+  return hasBeenAWhile;
 }
 
 export default Messages;
